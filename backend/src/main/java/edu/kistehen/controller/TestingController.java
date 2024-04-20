ac8d1b925f40b292b32346201c6f9716ec9ac27c
Simofi Levente <simofilevente@gmail.com>
2024-04-20 11:13:54 +0300

    Spring Rest initialized

---
+++ backend/src/main/java/edu/kistehen/controller/TestingController.java
@@ -0,0 +1,16 @@
+package edu.kistehen.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestingController {
+
+    @GetMapping
+    public String getTest() {
+        return "alma";
+    }
+}
