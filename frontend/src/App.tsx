eab1286fb84014cdab1446166bee8f3fa3b45afa
Hunor Fekete <hunor.csaba.fekete@stud.ubbcluj.ro>
2024-04-20 23:32:18 +0300

    added queryclient to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 import { Layout } from './Layout'
+import { queryClient } from './query/common.query'
 
 function App() {
 
@@ -10,7 +12,9 @@ function App() {
       {/* <div id="layout">
         <NavRoutes />
       </div> */}
+      <QueryClientProvider client={queryClient} >
         <Layout/>
+      </QueryClientProvider>
       {/* </SideBarContext.Provider> */}
     </>
   )
