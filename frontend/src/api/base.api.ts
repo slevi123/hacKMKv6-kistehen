dd0867bc3889b8ea8d0e82c26bc9b6bf04872bb6
Hunor Fekete <hunor.csaba.fekete@stud.ubbcluj.ro>
2024-04-20 22:38:29 +0300

    created base api and common query

--- frontend/src/api/base.api.ts
+++ frontend/src/api/base.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseApi = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'http://localhost:8080/api',
   headers: {
     Accept: 'application/json',
   },
